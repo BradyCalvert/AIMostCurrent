65777b522d79345c135486a3c84c5612f5b44b3d
svc-student <svc-student@student.directory.jccc>
2017-04-17 17:42:32 -0500

    master

---
+++ Assets/StateEnemyChaseV2.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class StateEnemyChaseV2 : StateMachineBehaviour {
+    GameObject player;
+    NavMeshAgent agent;
+    override public void OnStateEnter(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    {
+        player = animator.GetComponent<EnemyAIController>().player;
+        agent = animator.gameObject.GetComponent<NavMeshAgent>();
+    }
+
+    override public void OnStateUpdate(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    {
+
+        agent.SetDestination(player.transform.position);
+    }
+}
+
